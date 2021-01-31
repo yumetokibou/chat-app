3baeca082a611958479b7cf90d051009099f36db
yumetokibou <77270061+yumetokibou@users.noreply.github.com>
2021-01-31 17:41:55 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_092308) do
+ActiveRecord::Schema.define(version: 2021_01_31_072600) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_01_30_092308) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
