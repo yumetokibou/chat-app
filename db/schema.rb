584d8ec08d32e12283034b1a3e950c1189547262
yumetokibou <77270061+yumetokibou@users.noreply.github.com>
2021-02-01 22:48:28 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_01_31_084231) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
